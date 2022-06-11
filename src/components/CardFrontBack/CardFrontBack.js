8818c063a1924732936f7867c3e8460430108ddc
João no Desktop <joaoalencaramorim@gmail.com>
2022-06-11 14:56:05 -0300

    :sparkles: Add click event in CardFrontBack component

--- src/components/CardFrontBack/CardFrontBack.js
+++ src/components/CardFrontBack/CardFrontBack.js
@@ -3,8 +3,21 @@ import "./style.css";
 import CardGame from "../CardGame";
 
 function CardFrontBack() {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => { 
+    const $origin = event.target;
+    const $cardFrontBack = $origin.closest('.card-front-back');
+    $cardFrontBack.classList.toggle('-active');
+    /*
+    if ($cardFrontBack.classList.contains('-active')) {
+        $cardFrontBack.classList.remove('-active');
+    } else {
+        $cardFrontBack.classList.add('-active');
+    }       
+    */ 
+    }
     return /*html*/`
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
