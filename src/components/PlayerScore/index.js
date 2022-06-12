1e13d8e7128e69e52db891e72c4407c10caee4f9
João no Desktop <joaoalencaramorim@gmail.com>
2022-06-12 08:09:55 -0300

    :lipstick: Create Styles of the PlayerScore components

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -2,7 +2,7 @@ import "./style.css"
 
 function PlayerScore(points = 0) {
     return /*html*/`
-        <ol data-points=${points} class="player-score">
+        <ol data-points="${points}" class="player-score">
             <li class="score -score1"></li>
             <li class="score -score2"></li>
             <li class="score -score3"></li>
